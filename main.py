f2df08c71cc7729df9e8b032a484ddb35a75ab38
Christopher Thelin <cthelin21@gmail.com>
2018-04-04 17:48:21 -0600

    Added corrected stl files (origin is center of cubes). Added the visualizer files I've been using - they only work in Python 2.7 so far (PyOpenGL for Python 3 isn't playing nice)

---
+++ main.py
@@ -0,0 +1,130 @@
+import wx
+import visualizer
+try:
+    from wx import glcanvas
+
+    haveGLCanvas = True
+except ImportError:
+    haveGLCanvas = False
+
+try:
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
+    haveOpenGL = True
+except ImportError:
+    haveOpenGL = False
+
+class ButtonPanel(wx.Panel):
+    global canvases
+    def __init__(self, parent):     # parent is the frame from runTest
+        wx.Panel.__init__(self, parent, -1)
+
+        box = wx.BoxSizer(wx.VERTICAL)
+        box2 = wx.BoxSizer(wx.HORIZONTAL)
+        box3 = wx.BoxSizer(wx.VERTICAL)
+        box3_5 = wx.BoxSizer(wx.VERTICAL)
+        box4 = wx.BoxSizer(wx.HORIZONTAL)
+        box5 = wx.BoxSizer(wx.HORIZONTAL)
+        box6 = wx.BoxSizer(wx.HORIZONTAL)
+        box7 = wx.BoxSizer(wx.HORIZONTAL)
+
+        # btn_draw = wx.Button(self, 10, "Sync'd Draw")
+        # btn_draw = wx.ToggleButton(self, 10, "Sync'd Draw")
+        # box6.Add(btn_draw, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        # self.Bind(wx.EVT_BUTTON, self.SyncDraw, btn_draw)
+        # self.Bind(wx.EVT_TOGGLEBUTTON, self.SyncDraw, btn_draw)
+
+        # With this enabled, you see how you can put a GLCanvas on the wx.Panel
+        if 1:
+            context1 = visualizer.Model1Canvas(self)
+            context1.SetMinSize((400, 400))
+            box4.Add(context1, 0, wx.ALIGN_LEFT | wx.ALL, 5)
+
+        # add subboxes to boxes
+        box.Add(box2, 0, wx.ALIGN_CENTER | wx.TOP, 5)
+        box.Add(box5, 0, wx.ALIGN_CENTER | wx.TOP, 5)
+        box2.Add(box3, 0, wx.ALIGN_LEFT | wx.RIGHT, 5)
+        box2.Add(box3_5, 0, wx.ALIGN_LEFT | wx.ALL, 2)
+        box2.Add(box4, 0, wx.ALIGN_RIGHT | wx.LEFT, 5)
+        box5.Add(box6, 0, wx.ALIGN_LEFT | wx.RIGHT, 15)
+        box5.Add(box7, 0, wx.ALIGN_RIGHT | wx.LEFT, 15)
+        self.SetAutoLayout(True)
+        self.SetSizer(box)
+
+
+    def OnButton(self, evt):
+        if not haveGLCanvas:
+            dlg = wx.MessageDialog(self,
+                                   'The GLCanvas class has not been included with this build of wxPython!',
+                                   'Sorry', wx.OK | wx.ICON_WARNING)
+            dlg.ShowModal()
+            dlg.Destroy()
+
+        elif not haveOpenGL:
+            dlg = wx.MessageDialog(self,
+                                   'The OpenGL package was not found.  You can get it at\n'
+                                   'http://PyOpenGL.sourceforge.net/',
+                                   'Sorry', wx.OK | wx.ICON_WARNING)
+            dlg.ShowModal()
+            dlg.Destroy()
+
+        else:
+            canvasClassName = 'Model1Canvas'
+            canvasClass = eval(canvasClassName)
+            cx = 0
+            if canvasClassName == 'ConeCanvas':
+                cx = 400
+                # print "Making a Cone"
+            frame = wx.Frame(None, -1, canvasClassName, size=(400, 400), pos=(cx, 400))
+            canvasClass(frame)  # CubeCanvas(frame) or ConeCanvas(frame); frame passed to MyCanvasBase
+            frame.Show(True)
+
+
+# ----------------------------------------------------------------------
+class RunDemoApp(wx.App):
+    def __init__(self):
+        wx.App.__init__(self, redirect=False)
+
+    def OnInit(self):
+        frame = wx.Frame(None, -1, "RapidVizDemo: ", pos=(0, 0),
+                     style=wx.DEFAULT_FRAME_STYLE, name="run a sample")
+        # frame.CreateStatusBar()
+
+        menuBar = wx.MenuBar()
+        menu = wx.Menu()
+        item = menu.Append(wx.ID_EXIT, "E&xit\tCtrl-Q", "Exit demo")
+        self.Bind(wx.EVT_MENU, self.OnExitApp, item)
+        menuBar.Append(menu, "&File")
+
+        frame.SetMenuBar(menuBar)
+        frame.Show(True)
+        frame.Bind(wx.EVT_CLOSE, self.OnCloseFrame)
+
+        win = runTest(frame)    # win is a panel objext (ButtonPanel, to be precise)
+
+        # set the frame to a good size for showing the two buttons
+        frame.SetSize((600, 600))
+        win.SetFocus()  # sets panel to accept keyboard input
+        self.window = win
+        frect = frame.GetRect()
+
+        self.SetTopWindow(frame)    # tells app what the top window is
+        self.frame = frame
+        return True
+
+    def OnExitApp(self, evt):
+        self.frame.Close(True)
+
+    def OnCloseFrame(self, evt):
+        if hasattr(self, "window") and hasattr(self.window, "ShutdownDemo"):
+            self.window.ShutdownDemo()
+        evt.Skip()
+
+
+def runTest(frame):
+    win = ButtonPanel(frame)
+    return win
+
+
+app = RunDemoApp()
+app.MainLoop()
---
+++ visualizer.py
@@ -0,0 +1,507 @@
+import wx
+import os
+import sys
+import visualizer
+from load_stl import loader
+try:
+    from wx import glcanvas
+
+    haveGLCanvas = True
+except ImportError:
+    haveGLCanvas = False
+
+try:
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
+    haveOpenGL = True
+except ImportError:
+    haveOpenGL = False
+
+from OpenGLContext.arrays import *
+from PIL import Image
+import struct
+import matplotlib
+matplotlib.use('WXAgg')
+
+translate = 0.01
+zoom = 0.0
+rotx = 0.15
+translate_mode = False
+canvas1 = None
+canvas2 = None
+canvases = []
+
+linked_draw = False
+
+xPos = 0.0
+yPos = 0.0
+scaling = 0.025
+xRot = -90.0
+yRot = -30.0
+zRot = -45.0
+deltaRotX = 0.0
+deltaRotY = 0.0
+deltaRotZ = 0.0
+
+centerpoint = [0.0, 0.0, 0.0]
+
+AccumulatedRotation = []
+
+CurrentView = []
+
+Views = [None, None, None, None, None, None]
+
+
+class MyCanvasBase(glcanvas.GLCanvas):
+    def __init__(self, parent):
+        global canvases, linked_draw
+        glcanvas.GLCanvas.__init__(self, parent, -1)
+        self.init = False
+        self.context = glcanvas.GLContext(self)
+
+        self.id = 0
+
+        self.colors = [0, 1, 0, 1, 0, 0, 0, 0, 1]
+        self.vertices = [0.0, 2.0, 0.0, 0.0, -1.0, 2.0, -1.5, -1.0, -1.0]
+        self.indices = [0, 1, 2]
+        self.dif_colors = [0, 1, 0, 1, 0, 0, 0, 0, 1]
+        self.outlines_colors = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+        # initial mouse position
+        self.lastx = self.x = 30
+        self.lasty = self.y = 30
+        self.size = None
+        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnMouseDown)
+        self.Bind(wx.EVT_LEFT_UP, self.OnMouseUp)
+        self.Bind(wx.EVT_RIGHT_DOWN, self.OnRightDown)
+        self.Bind(wx.EVT_RIGHT_UP, self.OnRightUp)
+        self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
+        self.Bind(wx.EVT_MOUSEWHEEL, self.OnWheel)
+
+        canvases.append(self)
+
+    def OnEraseBackground(self, event):
+        pass  # Do nothing, to avoid flashing on MSW.
+
+    def OnSize(self, event):
+        wx.CallAfter(self.DoSetViewport)
+        event.Skip()
+
+    def DoSetViewport(self):
+        size = self.size = self.GetClientSize()
+        self.SetCurrent(self.context)
+        glViewport(0, 0, size.width, size.height)
+
+    def OnPaint(self, event):
+        dc = wx.PaintDC(self)
+        self.SetCurrent(self.context)
+        if not self.init:
+            self.InitGL()
+            self.init = True
+        self.OnDraw()
+
+    def OnMouseDown(self, evt):
+        self.CaptureMouse()
+        self.x, self.y = self.lastx, self.lasty = evt.GetPosition()
+
+    def OnMouseUp(self, evt):
+        self.ReleaseMouse()
+
+    def OnRightDown(self, evt):
+        self.CaptureMouse()
+        self.x, self.y = self.lastx, self.lasty = evt.GetPosition()
+
+    def OnRightUp(self, evt):
+        self.ReleaseMouse()
+
+    def OnMouseMotion(self, evt):
+        global translate_mode
+        global xRot, yRot, zRot, slider1x, slider1y
+        global xPos, yPos, scaling
+        global translate
+        global deltaRotX, deltaRotY, deltaRotZ
+
+        if evt.Dragging():
+            if evt.RightIsDown():
+                translate_mode = True
+            elif evt.LeftIsDown():
+                translate_mode = False
+            self.lastx, self.lasty = self.x, self.y
+            self.x, self.y = evt.GetPosition()
+
+            size = self.GetClientSize()
+            w, h = size
+            w = max(w, 1.0)
+            h = max(h, 1.0)
+            xScale = 180.0 / w
+            yScale = 180.0 / h
+
+            if translate_mode:
+                xPos += (self.x - self.lastx) * translate
+                yPos += (self.y - self.lasty) * translate * -1
+            elif self.y < (h / 10.0):
+                # zRot += (self.x - self.lastx) * xScale * -1
+                deltaRotZ += (self.x - self.lastx) * xScale * -1
+            else:
+                # xRot += (self.y - self.lasty) * yScale
+                # yRot += (self.x - self.lastx) * xScale
+                deltaRotX += (self.y - self.lasty) * yScale
+                deltaRotY += (self.x - self.lastx) * xScale
+
+            if linked_draw:
+                i = 1
+                for canvas in canvases:
+                    canvas.lastx, canvas.lasty = self.lastx, self.lasty
+                    canvas.x, canvas.y = self.x, self.y
+                    canvas.Refresh(False)
+                    i += 1
+            else:
+                self.Refresh(False)
+
+    def OnWheel(self, evt):
+        global zoom
+        global scaling
+        amt = evt.GetWheelRotation()
+        units = amt / (-(evt.GetWheelDelta()))
+        # scaling += units / 50.0
+        scaling += units / 80.0
+        # print units / 50.0
+        # print scaling
+        if scaling < 0:
+            scaling = 0
+
+        self.Refresh()
+        if linked_draw:
+            for canvas in canvases:
+                canvas.Refresh(False)
+        else:
+            self.Refresh(False)
+
+    def saveView(self, view_idx):
+        global xPos, yPos, scaling, AccumulatedRotation
+        global CurrentView, Views
+
+        CurrentView = [xPos, yPos, scaling, AccumulatedRotation]  # may need to add params for gluLookAt
+        Views[view_idx] = CurrentView
+        print "Save view"
+
+    def setView(self, view_idx):
+        global xPos, yPos, scaling, AccumulatedRotation
+        global CurrentView
+
+        # slider1x = CurrentView[0]
+        # slider1y = CurrentView[1]
+        # scaling = CurrentView[2]
+        # AccumulatedRotation = CurrentView[3]
+        if Views[view_idx] is not None:
+            xPos = Views[view_idx][0]
+            yPos = Views[view_idx][1]
+            scaling = Views[view_idx][2]
+            AccumulatedRotation = Views[view_idx][3]
+            print "Set view"
+        else:
+            print "No view saved for this button"
+
+
+        self.Refresh(False)
+
+    def screenshot(self):
+        size = self.GetClientSize()
+        w, h = size
+        # w = max(w, 1.0)
+        # h = max(h, 1.0)
+        data = glReadPixels(0, 0, w, h, GL_RGB, GL_UNSIGNED_BYTE)
+        # this 0, 0 is the lower LEFT corner... can I switch 0 for h?
+
+
+        ## this doesn't work because glReadPixels returns some kind of nonsense
+        # data_copy = list(data)
+        # for x in range(1, w):
+        #     for y in range(1, h):
+        #         data_copy[x,y] = data[x, h - y]
+
+        # image = Image.frombytes("RGB", (w, h), data)
+        image = Image.frombytes("RGB", (w, h), data)
+        image2 = image.transpose(Image.FLIP_TOP_BOTTOM)
+        image2.save('C:\Users\Chris\Documents\RepoOfAwesomeness\Temp\image' + str(self.id) + '.eps', 'EPS')
+        print "screenshot done"
+
+
+class Model1Canvas(MyCanvasBase):
+
+    def InitGL(self):
+        # global vbo
+        global shader
+        global AccumulatedRotation
+
+        # set viewing projection
+        glMatrixMode(GL_PROJECTION)  # now on the PROJECTION matrix stack, for setting up projection screen
+        glFrustum(-0.5, 0.5, -0.5, 0.5, 1.0, 40.0)  # (left, right, bottom, top, near, far)
+
+        # position viewer
+        glMatrixMode(GL_MODELVIEW)  # now on the MODELVIEW matrix stack, affects both MODEL and VIEW (same-ish)
+        glEnable(GL_DEPTH_TEST)
+
+        glLoadIdentity()
+        AccumulatedRotation = glGetFloatv(GL_MODELVIEW_MATRIX)
+
+        glClearColor(0.3, 0.3, 0.4, 0.0)
+
+        self.buffers = glGenBuffers(4)
+        self.BindTheBuffers()
+
+        self.parts = []
+        self.models = []
+        part_name = 'vw_3.stl'
+        scale1 = 0.5
+        scale2 = 1.0
+
+        # pos1 = [0.0, 0.0, 0.0]
+        rot1 = [0, 0, 0]
+        # pos2 = [10, 10, 0.0]
+        # rot2 = [45.0, 45.0, 90.0]
+        # pos1 = [0.0, 0.0, 0.0]
+        # pos2 = [0.0, 0.0, 0.0]
+        # pos3 = [1.0, 1.0, 0.0]*(10*scale1)
+        pos3 = [i*10*scale1 for i in [1.0, 1.0, 0.0]]
+        pos4 = [i*10*scale1 for i in [1.0, 2.0, 0.0]]
+        pos5 = [i*10*scale1 for i in [1.0, 3.0, 0.0]]
+        pos6 = [i*10*scale1 for i in [1.0, 1.0, 1.0]]
+        pos7 = [i*10*scale1 for i in [1.0, 3.0, 1.0]]
+        pos8 = [i*10*scale1 for i in [1.0, 3.0, 3.0]]
+        rot2 = [0, 180, 0]
+        rot3 = [0, 90, 0]
+        rot4 = [0, 270, 0]
+
+
+
+        # self.get_stl(part_name, pos1, rot1, scale1)
+        # self.get_stl(part_name, pos2, rot2, scale2)
+
+    # t-1.stl is macaroni in plane
+
+        self.get_stl('t-1c.stl', pos3, rot1, scale1)
+        # self.get_stl('t-1.stl', pos3, rot2, scale1)
+        self.get_stl('t-bc.stl', pos4, rot1, scale1)
+        self.get_stl('1-bc.stl', pos5, rot1, scale1)
+        self.get_stl('1-3c.stl', pos6, rot1, scale1)
+        self.get_stl('1-4c.stl', pos7, rot3, scale1)
+        self.get_stl('1-4c.stl', pos7, rot1, scale1)
+        self.get_stl('1-4c.stl', pos7, rot2, scale1)
+        self.get_stl('1-4c.stl', pos7, rot4, scale1)
+        # self.get_stl('1-4.stl', pos8, rot4, scale1)
+        self.init_shading()
+
+    def get_stl(self, model_name, pos, rot, scale):
+        model = loader()
+        model.model=[]  # for some reason new instance of loader has model information from last instance??
+
+        try:
+            model.load_stl(os.path.abspath('') + '/' + model_name)
+        except:
+            print "didn't work"
+        self.models.append(model)
+        self.assemble_part_list(model, pos, rot, scale)
+
+    def assemble_part_list(self, model, pos, rot, scale):
+        self.parts.append([model, pos, rot, scale])
+
+    def BindTheBuffers(self):
+        # global vertices, colors, indices
+        # global outlines_colors
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffers[0])
+        glBufferData(GL_ARRAY_BUFFER,
+                     len(self.vertices) * 4,
+                     (ctypes.c_float * len(self.vertices))(*self.vertices),
+                     GL_STATIC_DRAW)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffers[1])
+        glBufferData(GL_ARRAY_BUFFER,
+                     len(self.colors) * 4,
+                     (ctypes.c_float * len(self.colors))(*self.colors),
+                     GL_STATIC_DRAW)
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.buffers[2])
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                     len(self.indices) * 4,  # byte size
+                     (ctypes.c_uint * len(self.indices))(*self.indices),
+                     GL_STATIC_DRAW)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffers[3])
+        glBufferData(GL_ARRAY_BUFFER,
+                     len(self.outlines_colors) * 4,
+                     (ctypes.c_float * len(self.outlines_colors))(*self.outlines_colors),
+                     GL_STATIC_DRAW)
+
+    def init_shading(self):
+        glShadeModel(GL_SMOOTH)
+        # glClearColor(0.0, 0.0, 0.0, 0.0)
+        glClearDepth(1.0)
+        glEnable(GL_DEPTH_TEST)
+        glShadeModel(GL_SMOOTH)
+        glDepthFunc(GL_LEQUAL)
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+
+        glEnable(GL_COLOR_MATERIAL)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        # glLight(GL_LIGHT0, GL_POSITION, (0, 0, -100, 0))
+        glLight(GL_LIGHT0, GL_AMBIENT, (.3,.7,.7,0.5))
+        # glLight(GL_LIGHT0, GL_DIFFUSE, (0.5,0.5,0.5, 0.5))
+        # glEnable(GL_LIGHT1)
+        # glLight(GL_LIGHT1, GL_POSITION, (0, 0, -100, 0))
+        # glLight(GL_LIGHT0, GL_AMBIENT, (.7,.5,.3,1))
+        glMatrixMode(GL_MODELVIEW)
+
+    def OnDraw(self):
+        global translate, translate_mode
+        global zoom
+        global xPos, yPos, scaling, xRot, yRot, zRot
+        # global vertices, colors, indices
+        global centerpoint
+        global AccumulatedRotation
+        # global vbo
+        global deltaRotX, deltaRotY, deltaRotZ
+
+        # self.SetCurrent(self.context)
+        self.context.SetCurrent(self)
+        # print "begin context1"
+        # print self.context
+
+        # clear color and depth buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glLoadIdentity()
+        gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)  # initial point we look at (eye coords, center, up dir)
+        # gluLookAt(slider1x, -slider1y, scaling, slider1x, -slider1y, 0.0, 0.0, 1.0, 0.0)  # alternate way to do pan
+
+        # CENTERPOINT MARKER
+        glPushMatrix()
+        self.CenterpointMarker()
+        glPopMatrix()
+
+        # Apply initial transformations
+        glTranslatef(xPos, yPos, 0.0)   # this translate is for panning (the last transformation OpenGL will perform (besides the gluLookAt)
+
+        # DO ROTATIONS
+        glPushMatrix()  # Set aside the previous matrix
+        glLoadIdentity()    # Load an identity matrix (we call it Current Rotation)
+
+        # Apply the rotations to Current Rotation (not absolute, but deltas)
+        glRotatef(deltaRotX, 1.0, 0.0, 0.0)
+        glRotatef(deltaRotY, 0.0, 1.0, 0.0)
+        glRotatef(deltaRotZ, 0.0, 0.0, 1.0)
+
+        # Reset the deltas (or rotations will compound)
+        deltaRotX = 0
+        deltaRotY = 0
+        deltaRotZ = 0
+
+        glMultMatrixf(AccumulatedRotation)  # Multiply Current Rotation by the accumulated rotation matrix
+        AccumulatedRotation = glGetFloatv(GL_MODELVIEW_MATRIX)  # store the current result as Accumulated Rotation
+
+        glPopMatrix()   # Get back the first matrix we set aside earlier
+
+        glMultMatrixf(AccumulatedRotation)  # multiply the first matrix by Accumulated Rotation matrix
+
+        # Translate the object so its centerpoint rests on origin (OpenGL will apply this before the rotaitons)
+        glScalef(scaling, scaling, scaling)  # scale is essentially our zoom function (First transformation OpenGL will perform)
+        glTranslatef(-centerpoint[0], -centerpoint[1], -centerpoint[2])
+
+        # Start drawing
+
+        # glScale(-100,-100,-100)
+        glMaterial(GL_FRONT, GL_DIFFUSE, (0.8, 0.8, 0.8, 1.0))
+
+        i = 1.0
+        current_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+        for model in self.parts:
+            glPushMatrix()
+            glLoadIdentity()
+            glMultMatrixf(current_matrix)
+            # glTranslatef(model[1][0] + xPos, model[1][1] + yPos, model[1][2])
+            # glTranslatef(xPos, yPos, 0.0)
+            glTranslatef(model[1][0], model[1][1], model[1][2])
+            # glMultMatrixf(AccumulatedRotation)
+
+            glRotatef(model[2][0], 1.0, 0.0, 0.0)
+            glRotatef(model[2][1], 0.0, 1.0, 0.0)
+            glRotatef(model[2][2], 0.0, 0.0, 1.0)
+            # glScalef(scaling, scaling, scaling)
+            glScale(model[3], model[3], model[3])
+            # glTranslatef(-centerpoint[0], -centerpoint[1], -centerpoint[2])
+            model[0].draw()
+            i += 3.0
+
+            glPopMatrix()
+
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glEnableClientState(GL_COLOR_ARRAY)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffers[0])
+        glVertexPointer(3, GL_FLOAT, 0, None)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffers[1])
+        glColorPointer(3, GL_FLOAT, 0, None)
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.buffers[2])
+        glDrawElements(GL_TRIANGLES, len(self.indices), GL_UNSIGNED_INT, None)
+
+        # draw outlines
+        glLineWidth(2)
+        glPolygonMode(GL_FRONT, GL_LINE)
+        glPolygonMode(GL_BACK, GL_LINE)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffers[0])
+        glVertexPointer(3, GL_FLOAT, 0, None)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffers[3])
+        glColorPointer(3, GL_FLOAT, 0, None)
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.buffers[2])
+        glDrawElements(GL_TRIANGLES, len(self.indices), GL_UNSIGNED_INT, None)
+
+        glLineWidth(1)
+        glPolygonMode(GL_FRONT, GL_FILL)
+        glPolygonMode(GL_BACK, GL_FILL)
+
+        glDisableClientState(GL_COLOR_ARRAY)
+        glDisableClientState(GL_VERTEX_ARRAY)
+
+        # print "end context1"
+        self.SwapBuffers()
+
+    def CenterpointMarker(self):
+        z_position = -0
+        glBegin(GL_TRIANGLES)  # http://pyopengl.sourceforge.net/documentation/manual-3.0/glBegin.html
+
+        glColor3f(0.0, 0.0, 0.5)
+        glVertex3f(0.0, 0.0, 0.0 + z_position)
+        glVertex3f(0.0, 0.25, -1.0 + z_position)
+        glVertex3f(0.5, 0.0, -1.0 + z_position)
+
+        glColor3f(0.0, 0.5, 0.0)
+        glVertex3f(0.0, 0.0, 0.0 + z_position)
+        glVertex3f(0.0, 0.25, -1.0 + z_position)
+        glVertex3f(-0.5, 0.0, -1.0 + z_position)
+
+        glColor3f(0.5, 0.5, 0.5)
+        glVertex3f(0.0, 0.0, 0.0 + z_position)
+        glVertex3f(0.5, 0.0, -1.0 + z_position)
+        glVertex3f(-0.5, 0.0, -1.0 + z_position)
+
+        glColor3f(0.5, 0.5, 0.0)
+        glVertex3f(0.0, 0.25, -1.0 + z_position)
+        glVertex3f(0.5, 0.0, -1.0 + z_position)
+        glVertex3f(-0.5, 0.0, -1.0 + z_position)
+        glEnd()
+
+
+
